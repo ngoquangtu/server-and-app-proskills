06a34ac896b71b25f26b74eb5bba74f66c5a7ab9
ngoquangtu <ngoquangtuhd@gmail.com>
2024-05-29 22:37:21 +0700

    retest ci

--- web-app-frontend/src/components/adminComponent.jsx
+++ web-app-frontend/src/components/adminComponent.jsx
@@ -14,6 +14,7 @@ token={token}
       <h1>Admin Component</h1>
       <h2>Admin Role</h2>
       <h3> Test ci/cd</h3>
+      <h3>1234</h3>
       <CommentFormComponent courseId={courseId}  />
       <CommentListComponent  />
     </div>
