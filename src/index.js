866dab76039e0a7d366a0609b36b940322187d3b
ngoquangtu <ngoquangtuhd@gmail.com>
2024-06-15 13:37:38 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+
+
+const rateLimit = require('express-rate-limit');
+
 const app=express();
 
 app.use(cors());  
@@ -19,12 +23,19 @@ const courses = require('./routes/api-course');
 const users=require('./routes/api-user.js')
 const admin=require('./routes/api-admin.js');
 const youtube=require('./routes/api-youtube.js');
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,// limit number of request in 1 min
+  message: 'Too many requests from this IP, please try again later.'
+});
+
 app.use('/api/courses',courses);
-app.use('/api/auth', authentication);
+app.use('/api/auth',limiter, authentication);
 app.use('/api/comments',comments);
 app.use('/api/courses', courses);
 app.use('/api/users',users);
-app.use('/api/admin',admin);
+app.use('/api/admin',limiter,admin);
 app.use('/api/youtube',youtube);
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
